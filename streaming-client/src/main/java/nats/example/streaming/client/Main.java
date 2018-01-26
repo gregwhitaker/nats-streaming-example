883bf429c39e73427e9630c8ce3b35226070da8b
gregwhitaker <gwhitake@gmail.com>
2018-01-26 09:22:53 -0800

    Initial Commit

---
+++ streaming-client/src/main/java/nats/example/streaming/client/Main.java
@@ -0,0 +1,8 @@
+package nats.example.streaming.client;
+
+public class Main {
+
+    public static void main(String... args) throws Exception {
+
+    }
+}
---
+++ streaming-service/src/main/java/nats/example/streaming/service/Main.java
@@ -0,0 +1,8 @@
+package nats.example.streaming.service;
+
+public class Main {
+
+    public static void main(String... args) throws Exception {
+
+    }
+}
